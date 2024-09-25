6e796fa16f04159e812cdc6120360468753d7928
unknown <yuyu14191567@gmail.com>
2024-09-26 01:00:19 +0900

    Charactar_move_Update

--- Assets/Scripts/Charactar_move.cs
+++ Assets/Scripts/Charactar_move.cs
@@ -17,46 +17,73 @@ public class Charactar_move : MonoBehaviour
     private float span;
     private int m;
     private GameObject a;
-
-    private Vector3 addposition;
-    private GameObject b;
     private Vector3 target;
-
+    private LineRenderer lineRenderer;
+    private int currentSegment = 0;
+    private float t = 0f; 
+    public float duration = 5f;
+    public bool canMove;
+    private float delayspan;
+    private int k;
     // Start is called before the first frame update
     void Start()
     {
-
+        canMove = true;
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
         i = 0;
         true_musical_scale_List = musical_scales_List.true_musical_scales;
-
         m = 0;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        SpeedTime += Time.deltaTime;
-        span += Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.A))
+        for(int k = 0; k<=true_musical_scale_List.Count-1;++k)
         {
-            Check(text);
+        Instancewall(k);
         }
-        // if (span >= 3)
-        // {
-        //     Check(true_musical_scale_List[m]);
-        //     m += 1;
-        //     span = 0;
-        //     SpeedTime = 0;
-        // }
-        gameObject.transform.position = gameObject.transform.position + addposition;
-
     }
+void Update()
+{
+  
+    span += Time.deltaTime;
+
+    if (Input.GetKeyDown(KeyCode.A))
+    {
+        Check(text);
+    }
+    
+
+    delayspan += Time.deltaTime;
+    if(canMove == true)
+    {
+     if(SpeedTime >= 0)
+   {
+      SpeedTime -= Time.deltaTime;
+        if (currentSegment < lineRenderer.positionCount - 1)
+        {
+            Vector3 startPosition = lineRenderer.GetPosition(currentSegment);
+            Vector3 endPosition = lineRenderer.GetPosition(currentSegment + 1);
+            transform.position = Vector3.Lerp(startPosition, endPosition, t);
+            t += Time.deltaTime;
+
+            if (t >= 1f)
+            {
+                t = 0f;
+                currentSegment++;
+                canMove = false;
+            }
+        }
+        else
+        {
+            
+            currentSegment = 0; 
+        }
+   }
+    }
+    
+}
+
     public void Check(string musical_scale)
     {
-        SpeedTime = 0;
         string true_musical_scale = true_musical_scale_List[i];
         i += 1;
-        Instancewall(i +1);
+        Instancewall(i + 1);
         if (true_musical_scale_List[i] != "休符")
         {
             List<string> musical_scale_types = new List<string>() { "ラ4", "シ4", "ド5", "レ5", "ミ5", "ファ5", "ソ5", "ラ5", "シ5", "ド6", "レ6" };
@@ -64,22 +91,22 @@ public class Charactar_move : MonoBehaviour
             {
                 if (musical_scale == musical_scale_types[0] || musical_scale == musical_scale_types[1] || musical_scale == musical_scale_types[10])
                 {
-                    Debug.Log("seikou");
+                    speed += 0.3f;
                 }
                 else
                 {
-                    Debug.Log("失敗");
+                  canMove = false;
                 }
             }
             else if (musical_scale_types[10] == true_musical_scale)
             {
                 if (musical_scale == musical_scale_types[0] || musical_scale == musical_scale_types[9] || musical_scale == musical_scale_types[10])
                 {
-                    Debug.Log("seikou");
+                    speed += 0.3f;
                 }
                 else
                 {
-                    Debug.Log("失敗");
+                   canMove = false;
                 }
             }
             else
@@ -90,56 +117,54 @@ public class Charactar_move : MonoBehaviour
                     {
                         if (musical_scale == musical_scale_types[j] || musical_scale == musical_scale_types[j - 1] || musical_scale == musical_scale_types[j + 1])
                         {
+                            speed += 0.3f;
                             if (true_musical_scale == "ド5")
                             {
-
-                                addposition = new Vector3(0.01f, 0.01f, 0.02f) * speed;
+                             
                             }
                             else if (true_musical_scale == "レ5")
                             {
-
-                                addposition = new Vector3(-0.01f, -0.01f, 0.02f) * speed;
+                           
                             }
                             else if (true_musical_scale == "ミ5")
                             {
-
-                                addposition = new Vector3(-0.01f, 0, 0.02f) * speed;
+                            
                             }
                             else if (true_musical_scale == "ファ5")
                             {
-
-                                addposition = new Vector3(0, 0.01f, 0.02f) * speed;
-
+                            
                             }
                             else if (true_musical_scale == "ソ5")
                             {
-
-                                addposition = new Vector3(0.01f, 0, 0.02f) * speed;
-
+               
                             }
                             else if (true_musical_scale == "ラ5")
                             {
-
-                                addposition = new Vector3(0, -0.01f, 0.02f) * speed;
+                     
                             }
                             else if (true_musical_scale == "シ5")
                             {
-
-                                addposition = new Vector3(0.01f, 0.01f, 0.02f) * speed;
-
+                    
                             }
                             else if (true_musical_scale == "ド6")
                             {
-
-                                addposition = new Vector3(0.01f, 0.01f, 0.02f) * speed;
+      
+                            }
+                            else if (true_musical_scale == "シ4")
+                            {
+   
+                            }
+                            else if (true_musical_scale == "レ6")
+                            {
 
                             }
+                            speed += 0.3f;
                             Debug.Log("seikou");
                             break;
                         }
                         else
                         {
-                            Debug.Log("失敗");
+                           canMove = false;
                             break;
                         }
                     }
@@ -154,70 +179,107 @@ public class Charactar_move : MonoBehaviour
             }
             else
             {
-                Debug.Log("失敗");
+            canMove = false;
             }
         }
 
     }
     void Instancewall(int k)
     {
-        a = Instantiate(Cloud, gameObject.transform.position + new Vector3(0, 0, 54), Quaternion.identity);
+        lineRenderer.positionCount = k + 1;
+        a = Instantiate(Cloud, target + new Vector3(0, 0, 40), Quaternion.identity);
         string true_musical_scale = true_musical_scale_List[k];
         Up = a.transform.Find("Up").gameObject;
         Left = a.transform.Find("Left").gameObject;
         Right = a.transform.Find("Right").gameObject;
         Down = a.transform.Find("Down").gameObject;
-
         if (true_musical_scale == "ド5")
         {
+            lineRenderer.SetPosition(k, target + new Vector3(15, -15, 40));
+            target = target + new Vector3(15, -15, 40);
             Destroy(Down);
             Destroy(Right);
-          
+            Destroy(Up);
         }
         else if (true_musical_scale == "レ5")
         {
-
+            lineRenderer.SetPosition(k, target + new Vector3(-15, -15, 40));
+            target = target + new Vector3(-15, -15, 40);
             Destroy(Down);
             Destroy(Left);
-         
+            Destroy(Right);
         }
         else if (true_musical_scale == "ミ5")
         {
             Destroy(Left);
-          
+            Destroy(Right);
+            Destroy(Up);
+            lineRenderer.SetPosition(k, target + new Vector3(-15, 0, 40));
+            target = target + new Vector3(-15, 0, 40);
+
         }
         else if (true_musical_scale == "ファ5")
         {
             Destroy(Up);
-           
+            Destroy(Left);
+            Destroy(Right);
+            lineRenderer.SetPosition(k, target + new Vector3(0, 15, 40));
+            target = target + new Vector3(0, 15, 40);
 
         }
         else if (true_musical_scale == "ソ5")
         {
             Destroy(Right);
-        
+            Destroy(Left);
+            Destroy(Right);
+            lineRenderer.SetPosition(k, target + new Vector3(15, 0, 40));
+            target = target + new Vector3(15, 0, 40);
 
         }
         else if (true_musical_scale == "ラ5")
         {
+            lineRenderer.SetPosition(k, target + new Vector3(0, -15, 40));
+            target = target + new Vector3(0, 15, 40);
             Destroy(Down);
-    
+            Destroy(Right);
+            Destroy(Left);
+
         }
         else if (true_musical_scale == "シ5")
         {
+            lineRenderer.SetPosition(k, target + new Vector3(-15, 15, 40));
+            target = target + new Vector3(-15, 15, 40);
             Destroy(Left);
             Destroy(Up);
-          
-
+            Destroy(Right);
         }
         else if (true_musical_scale == "ド6")
         {
+            lineRenderer.SetPosition(k, target + new Vector3(15, 15, 40));
+            target = target + new Vector3(15, 15, 40);
             Destroy(Right);
             Destroy(Down);
-         
+            Destroy(Left);
         }
-
+        else if (true_musical_scale == "ラ4")
+        {
+            lineRenderer.SetPosition(k, target + new Vector3(0, 0, 40));
+            Destroy(a);
+        }
+        else if (true_musical_scale == "シ4")
+        {
+            lineRenderer.SetPosition(k, target + new Vector3(0, 0, 40));
+              Destroy(a);
+        }
+        else if (true_musical_scale == "レ6")
+        {
+            lineRenderer.SetPosition(k, target + new Vector3(0, 0, 40));
+              Destroy(a);
+        }
+        else if(true_musical_scale == "休符")
+        {
+            lineRenderer.SetPosition(k, target + new Vector3(0, 0,40));
+            Destroy(a);
+        }
     }
-
-
 }
