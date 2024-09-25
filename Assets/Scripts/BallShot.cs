de983ae24389a1ddc9b3a995efe162f2ca4d938e
unknown <yuyu14191567@gmail.com>
2024-09-26 08:24:23 +0900

    Demo_game

--- Assets/Scripts/BallShot.cs
+++ Assets/Scripts/BallShot.cs
@@ -6,14 +6,21 @@ public class BallShot : MonoBehaviour
 {
     [SerializeField] GameObject star;
     private float speed = 300;
+    public Transform player_pos;
 
     void Update()
     {
-        if (Input.GetKeyDown("space"))
+        if(Input.GetKeyDown("space"))
         {
-            GameObject ball = (GameObject)Instantiate(star, transform.position, Quaternion.identity);
-            Rigidbody ballRigidbody = ball.GetComponent<Rigidbody>();
-            ballRigidbody.AddForce(-transform.forward * speed);
+         Death();
+         Debug.Log("cv");
         }
+ 
+    }
+    public void Death()
+    {
+            GameObject ball =Instantiate(star, player_pos.transform.position +new Vector3(0,0,100), Quaternion.identity);
+            Rigidbody ballRigidbody = ball.GetComponent<Rigidbody>();
+            ballRigidbody.velocity = new Vector3(0,0,-100);
     }
 }
